29789379db1c1c7d85fdd91f3bfbc198f6310794
Rny0Raneem <rny0raneem@gmail.com>
2024-02-23 05:26:04 +0300

    Update index.php

--- new-web/index.php
+++ new-web/index.php
@@ -11,6 +11,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <body>
         <?php
             echo 'hellow world';
+            echo "<br>from Raneem";
         ?>
     </body>
 </html>
